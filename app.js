94acadb381f12359722b14868b03db03320187a3
Fer2000-ita <fers200devillers@gmail.com>
2023-02-15 09:45:21 -0600

    Agregar

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let listaEmpleados=[];
+
 const objEmpleado ={
-    id:'',
-    nombre:'',
-    apellido:''
+    id: '',
+    nombre: '',
+    apellido: ''
 }
+
 let editando = false;
-const formulario = document.querySelector('#formulario');
+const formulario = document.querySelector('#Formulario');
 const nombreImput =document.querySelector('#nombre');
 const apellidoImput = document.querySelector('#apellido');
 const BTNagregar =document.querySelector('#BTNagregar');
@@ -13,8 +15,42 @@ const BTNagregar =document.querySelector('#BTNagregar');
 formulario.addEventListener('submit', validarformulario);
 function validarformulario(e){
     e.preventDefault();
-    if(nombreImput.value === '' || puestoImput.value === ''){
+    if(nombreImput.value === ''|| apellidoImput.value === ''){
         alert('Todos los campos son obligatorios.');
         return;
     }
+    if(editando){
+        //editarEmpleado();
+        editando = false;
+    } else {
+        objEmpleado.id = Date.now();
+        objEmpleado.nombre=nombreImput.value;
+        objEmpleado.apellido=apellidoImput.value;
+
+        agregarEmpleado();
+    }
+}
+
+function agregarEmpleado(){
+    listaEmpleados.push({...objEmpleado});
+    
+    mostrarEmpleados();
+}
+
+function mostrarEmpleados(){
+    const divEmpleados = document.querySelector('.div_empleados');
+     listaEmpleados.forEach(empleado =>{
+        const {id, nombre, apellido} =empleado;
+
+        const parrafo=document.createElement('p');
+        parrafo.textContent='${id} - ${nombre} - ${p} -';
+        parrafo.dataset.id= id;
+
+        const editarBotton = document.createElement('button');
+        //editarBotton.onclick=()=> cargarEmpleado(empleado);
+        editarBotton.textContent='Editar';
+        editarBotton.classList.add('btn','btn-editar');
+        parrafo.append(editarBotton);
+
+     });
 }
